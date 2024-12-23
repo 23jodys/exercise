46f58e9e8190c7700de36d8db0ed6888af2310ad
Jody Stephens <jodys@helluin.org>
2024-12-23 08:01:27 -0700

    Refactor and enhance overlap graph functionality
    - Implement `_determine_if_overlapping` function to check overlap between two sequences.
    - Develop `ovl_overlap_graph` to construct an overlap graph from input sequences.
    - Add `ovl_sort_insertion` function to sort nodes using insertion sort.
    - Implement `ovl_add`, `ovl_init`, and `ovl_free` for managing overlap list lifecycle.
    - Introduce `ovl_node_sdsprintf` and `ovl_debug_list` for debugging and string formatting.
    - Minor optimizations and bug fixes.

--- src/grph.c
+++ src/grph.c
@@ -2,7 +2,7 @@
 #include "libfasta.h"
 
 
-bool determine_if_overlapping(sds string1, sds string2, int len) {
+bool _determine_if_overlapping(sds string1, sds string2, int len) {
 	/* String 1 or 2 to short, not overlapping by definition */
 	if (sdslen(string1) < len || sdslen(string2) < len) {
 		return false;
@@ -28,8 +28,8 @@ bool determine_if_overlapping(sds string1, sds string2, int len) {
 	return overlapping;
 }
 
-int make_overlap_graph(FastaStrings* input, GraphAdjacencyNode* nodes, int overlap) {
-	int node_len = 0;
+ovl_List* ovl_overlap_graph(FastaStrings* input, int overlap) {
+	ovl_List* overlap_graph = ovl_init();
 
 	for (int i=0; i < input->len; i++) {
 		for (int j=0; j < input->len; j++) {
@@ -37,34 +37,197 @@ int make_overlap_graph(FastaStrings* input, GraphAdjacencyNode* nodes, int overl
 				debug("i = %d, j = %d", i, j);
 				debug("name1: %s", input->sequences[i].sequence);
 				debug("name2: %s", input->sequences[j].sequence);
-				if (determine_if_overlapping(input->sequences[i].sequence, input->sequences[j].sequence, overlap)) {
-					nodes[node_len].name1 = input->sequences[i].name;
-					nodes[node_len].name2 = input->sequences[j].name;
-					node_len += 1;
-					debug("%s overlapped %s", nodes[node_len].name1, nodes[node_len].name2);
+				if (_determine_if_overlapping(
+					input->sequences[i].sequence,
+					input->sequences[j].sequence, overlap)
+				) {
+
+					overlap_graph = ovl_add(
+						overlap_graph,
+						input->sequences[i].name,
+						input->sequences[j].name
+					);
+
+					debug(
+						"%s overlapped %s",
+						input->sequences[i].name,
+						input->sequences[j].name
+					);
 				}
 			}
 		}
 	}
-	return node_len;
+	return overlap_graph;
+}
+
+ovl_List* ovl_sort_insertion(ovl_List* list) {
+	if (list == NULL || 1 == list->size) {
+		/* Trivially true, so return as is */
+		return list;
+	}
+
+	/* Pointer to the partition of the list that has been sorted */
+	ovl_Node* sorted = NULL;
+
+	/* Pointer to select nodes from the unsorted partition of the list */
+	ovl_Node* curr = list->head;
+
+	while (curr != NULL) {
+		ovl_Node* next = curr->next;
+
+		if (sorted == NULL || ovl_node_cmp(sorted, curr) >= 0) {
+			debug(
+				"Already sorted inserting curr '%s' before sorted '%s'",
+				ovl_node_sdsprintf(curr),
+				sorted == NULL ? "NULL" : ovl_node_sdsprintf(sorted)
+			);
+			curr->next = sorted;
+			if (sorted != NULL) sorted->prev = curr;
+			sorted = curr;
+			sorted->prev = NULL;
+		} else {
+			ovl_Node* current_sorted = sorted;
+
+			while(current_sorted->next != NULL && ovl_node_cmp(current_sorted->next, curr) < 0) {
+				current_sorted = current_sorted->next;
+			}
+			ovl_debug_list(sorted);
+			debug(
+				"Going to insert curr '%s' after current_sorted '%s'",
+				ovl_node_sdsprintf(curr),
+				ovl_node_sdsprintf(current_sorted)
+			);
+
+			curr->next = current_sorted->next;
+
+			if(current_sorted->next != NULL) {
+				current_sorted->next->prev = curr;
+			}
+
+			current_sorted->next = curr;
+			curr->prev = current_sorted;
+		}
+		curr = next;
+	}
+	list->head = sorted;
+	return list;
+}
+
+sds ovl_node_sdsprintf(ovl_Node* to_print) {
+	sds result = sdsempty();
+	return sdscatprintf(result, "%s %s", to_print->name1, to_print->name2);
+}
+
+void ovl_debug_list(ovl_Node* head) {
+	int counter = 0;
+	while(head != NULL) {
+		debug("%d: %s %s", counter, head->name1, head->name2);
+		head = head->next;
+		counter++;
+	}
+}
+
+
+int ovl_node_cmp(ovl_Node* node1, ovl_Node* node2) {
+	int compare = strcmp(node1->name1, node2->name1);
+	if (compare == 0) {
+		compare = strcmp(node1->name2, node2->name2);
+	}
+	/*
+	debug(
+			"Comparing '%s' to '%s' and going to return %d",
+			ovl_node_sdsprintf(node1),
+			ovl_node_sdsprintf(node2),
+			compare
+	);
+	*/
+	return compare;
+}
+
+ovl_List* ovl_diff(ovl_List* nodes1, ovl_List* nodes2) {
+	return nodes1;
 
 }
 
+ovl_List* ovl_add(ovl_List* list, sds name1, sds name2) {
+	/* malloc space for new node */
+	ovl_Node* new_tail = malloc(sizeof(ovl_Node));
+
+	/* Locate old tail node, update to point to new node */
+	ovl_Node* old_tail = list->tail;
+	if (old_tail) {
+		old_tail->next = new_tail;
+	} else {
+		/* if old_tail is NULL then it means we are in an empty list.
+		 * This means we need to point head and tail to our new node
+		 */
+		list->head = new_tail;
+	}
+
+	/* update new node to point to prior tail */
+	new_tail->name1 = name1;
+	new_tail->name2 = name2;
+	new_tail->next = NULL;
+	new_tail->prev = old_tail;
+
+	/* update list node with new tail */
+	list->tail = new_tail;
+
+	/* increment the size */
+	list->size += 1;
+
+	return list;
+}
+
+ovl_List* ovl_init(void) {
+	/* malloc list metadata */
+	ovl_List* result = malloc(sizeof(ovl_List));
+
+	/* set size 0 */
+	result->size = 0;
+	result->tail = NULL;
+	result->head = NULL;
+
+	return result;
+}
 
 sds grph_rosalind_interface(FILE* stream) {
 	FastaStrings* input = FastaStrings_fromFile(stream);
 	log_info("Found %d sequences", input->len);
 
-	int graph_len;
-
-	GraphAdjacencyNode nodes[100000];
-
-	graph_len = make_overlap_graph(input, nodes, 3);
-
-	for (int i = 0; i < graph_len; i++) {
-		printf("%s %s\n", nodes[i].name1, nodes[i].name2);
-	}
+	ovl_List* overlap_graph = ovl_overlap_graph(input, 3);
 
 	sds result = sdsempty();
+	ovl_Node* curr = overlap_graph->head;
+	while(curr != NULL) {
+		result = sdscatsds(result, ovl_node_sdsprintf(curr));
+		curr = curr->next;
+	}
+
 	return result;
+}
+
+void ovl_free(ovl_List** list) {
+	ovl_Node* curr = (*list)->tail;
+
+	/* walk from the tail to the head and free nodes */
+	while(curr != NULL) {
+		/* free the sds strings */
+		sdsfree(curr->name1);
+		sdsfree(curr->name2);
+
+		ovl_Node* to_free = curr;
+		curr = curr->next;
+
+		/* free this node */
+		free(to_free);
+	}
+
+	/* free the list structure */
+	free(*list);
+
+	/* set pointer to null */
+	*list = NULL;
+
+	
 }
